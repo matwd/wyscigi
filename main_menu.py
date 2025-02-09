6fcf67e7debbc364e459a26dd31cdc4d62f4a236
wiktorsiwecki <wiktorsiwecki123@gmail.com>
2025-02-09 23:09:24 +0100

    added button class, made game start on button press

--- main_menu.py
+++ main_menu.py
@@ -1,15 +1,21 @@
 import pygame
+from button import Button
 
 class MainMenu:
     def __init__(self, game):
+        
         self.game = game
-        self.font = pygame.freetype.SysFont(pygame.freetype.get_default_font(), 40)
+        self.font = pygame.font.SysFont(None, 40)
+        self.play_button = Button(pos=(200,300), text_var="GRAJ", font=self.font, text_color=(255, 0, 0),hover_color=(255, 141, 161))
 
     def update(self, events):
+        mouse_pos = pygame.mouse.get_pos()
+        self.play_button.changeColor(mouse_pos)
         for event in events:
-            if event.type == pygame.KEYDOWN:
-                self.game.start_race(0, 0)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.play_button.checkForInput(mouse_pos):
+                    self.game.start_race(0, 0)
+
 
     def draw(self):
-        text_surface, ract = self.font.render("Kliknij dowolny klawisz aby zacząć", pygame.color.THECOLORS["white"], size=0)
-        self.game.screen.blit(text_surface, (40, 300))
+        self.play_button.update(self.game.screen)

d6dbb0b96fdd0b52fdab51ab1dc5b7854e0aa663
maim <zbigniew.mateusz12.gmail.com>
2025-03-24 12:09:54 +0100

    nowy skrypt do kompilowania aplikacji

---
+++ build.bat
@@ -0,0 +1,6 @@
+echo linijka
+@REM .venv\Scripts\activate.bat
+echo linijka1
+pyinstaller --paths .\.venv\Lib\site-packages --hidden-import "pygame.freetype" main.py -y
+echo linijka2
+xcopy .\assets .\dist\main\assets /s /e /h /I

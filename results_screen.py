d69fbdb467789029374d7e4dccf448816a08943d
mikey <michal_maslanka@onet.pl>
2025-03-29 18:01:29 +0100

    fix

--- results_screen.py
+++ results_screen.py
@@ -93,7 +93,7 @@ class ResultsScreen:
             with open("results.json", "r") as file:
                 results = json.load(file)
         except:
-            with open("results.json", "r") as file:
+            with open("results.json", "w") as file:
                 json.dump(results, file)
 
         ranking = results[f"map{self.game.selected_map}"]
@@ -123,10 +123,14 @@ class ResultsScreen:
             time_surface = self.font.render(f"{float(result['time']):.2f}", True, (255, 255, 255))
             self.game.screen.blit(time_surface, (self.screen_width // 4 + self.resultsBg.width - time_surface.get_width() - 20, self.screen_height // 4 + 50 * (i % self.results_per_page) + self.title_text.get_height() + 10))
 
-        page_text = self.font.render(f"Page {self.page + 1}/{self.max_page + 1}", True, (255, 255, 255))
-        self.game.screen.blit(page_text, (self.screen_width // 2 - page_text.get_width() // 2 , self.screen_height // 4 + 50 * self.results_per_page + self.title_text.get_height() + 10))
+        if len(ranking) == 0:
+            no_results = self.font.render("No results", True, (255, 255, 255))
+            self.game.screen.blit(no_results, (self.screen_width // 2 - no_results.get_width() // 2, self.screen_height // 4 + 50 * self.results_per_page + self.title_text.get_height() + 10))
+        else:
+            page_text = self.font.render(f"Page {self.page + 1}/{self.max_page + 1}", True, (255, 255, 255))
+            self.game.screen.blit(page_text, (self.screen_width // 2 - page_text.get_width() // 2 , self.screen_height // 4 + 50 * self.results_per_page + self.title_text.get_height() + 10))
 
-        self.prev_page_button.draw(self.game.screen)
-        self.next_page_button.draw(self.game.screen)
+            self.prev_page_button.draw(self.game.screen)
+            self.next_page_button.draw(self.game.screen)
 
 

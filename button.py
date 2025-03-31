4e52522daa653557b4b3091dfbb7ff36d8d8caaa
wiktorsiwecki <wiktorsiwecki123@gmail.com>
2025-03-31 21:14:31 +0200

    more forgiving button "hitbox"

--- button.py
+++ button.py
@@ -38,7 +38,7 @@ class Button():
         Obsługa naciśnięcia przycisku.
         """
         position = [round(position[0]/self.real_screen.get_size()[0] * 1920), round(position[1] / self.real_screen.get_size()[1] * 1080)]
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(int(self.rect.top + (self.rect.bottom - self.rect.top) / 4), int(self.rect.bottom- (self.rect.bottom - self.rect.top) / 4)):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(int(self.rect.top + (self.rect.bottom - self.rect.top) / 4), int(self.rect.bottom- (self.rect.bottom - self.rect.top) / 5)):
             return True
         return False
     
@@ -47,7 +47,7 @@ class Button():
         Obsługa zmiany koloru przycisku po najechaniu
         """
         position = [round(position[0]/self.real_screen.get_size()[0] * 1920), round(position[1] / self.real_screen.get_size()[1] * 1080)]
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(int(self.rect.top + (self.rect.bottom - self.rect.top) / 4), int(self.rect.bottom- (self.rect.bottom - self.rect.top) / 4)) and not hide:
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(int(self.rect.top + (self.rect.bottom - self.rect.top) / 4), int(self.rect.bottom- (self.rect.bottom - self.rect.top) / 5)) and not hide:
             # jeżeli najechano na przycisk i nie jest on ukryty, to zmienia kolor na hover_color sprecyzowany w konstruktorze
             self.text = self.font.render(self.text_var, True, self.hover_color)
         elif hide:
